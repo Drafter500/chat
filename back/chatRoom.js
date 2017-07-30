1cbdbbc939fdb1345afff706779bd37903cd94d3
Drafter500 <vzemtsov@yahoo.com>
2017-07-30 13:12:26 +0300

    server code refactoring

---
+++ back/chatRoom.js
@@ -0,0 +1,26 @@
+import socketIo from 'socket.io';
+import cookie from 'cookie';
+import jwt from 'jsonwebtoken';
+import { TOKEN_KEY } from './constants';
+
+
+export default function initializeChat(server) {
+  const io = socketIo.listen(server);
+
+  const participants = [];
+
+  io.on('connection', (socket) => {
+    const cookieParsed = cookie.parse(socket.request.headers.cookie);
+    const credentials = jwt.decode(cookieParsed[TOKEN_KEY]);
+
+    // TODO: process credentials, check if user exists, if not, add him to the list
+    participants.push(credentials);
+    //io.emit('participants updated', participants);
+
+    console.log('user connected');
+    const user = credentials.username;
+    socket.on('chat message', (msg) => {
+      io.emit('message arrived', `${user}: ${msg}`);
+    });
+  });
+}
--- back/server.babel.js
+++ back/server.babel.js
@@ -1,10 +1,12 @@
-const path = require('path');
-const bodyParser = require('body-parser');
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
-const cookie = require('cookie');
-const cookieParser = require('cookie-parser');
+import path from 'path';
+import bodyParser from 'body-parser';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import expressJwt from 'express-jwt';
+import cookieParser from 'cookie-parser';
+import chatRoom from './chatRoom';
+import { TOKEN_KEY } from './constants';
+
 
 const app = express();
 app.use(expressJwt({ secret: 'the real true secret' }).unless({ path: ['/', '/bundle.js', '/login', '/users'] }));
@@ -14,31 +16,17 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running');
 });
 
-const io = require('socket.io').listen(server);
-
-const TOKEN_KEY = 'auth_token';
+// Initialize chat wiht sockets
+chatRoom(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  const cookieParsed = cookie.parse(socket.request.headers.cookie);
-  const credentials = jwt.decode(cookieParsed[TOKEN_KEY]);
-
-  // TODO: process credentials, check if user exists, if not, add him to the list
-
-  console.log('user connected');
-  const user = credentials.username;
-  socket.on('chat message', (msg) => {
-    io.emit('message arrived', `${user}: ${msg}`);
-  });
-});
 // app.all('*', (req, res) => res.sendFile(path.resolve(__dirname, '../public/index.html')));
 
 
 app.get('/roomInfo', (req, res) => {
-  res.status(200).send('thiы is the protected room info');
+  res.status(200).send('this is the protected room info');
 });
 
 app.post('/login', (req, res) => {
